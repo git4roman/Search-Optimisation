69fb5a997661bb6adc5b22eab3bd679d38b8b324
Roman Khatri <romanforgit@gmail.com>
2025-10-27 16:44:08 +0545

    3 tier setup

---
+++ SO.Core/UserEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace SO.Core;
+
+public class UserEntity
+{
+    protected UserEntity()
+    {
+        
+    }
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string College { get; set; }
+    public string University { get; set; }
+    public string TechStack { get; set; }
+    public string Program { get; set; }
+    public string Address { get; set; }
+}
---
+++ SO.Data/UserEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SO.Core;
+
+namespace SO.Data;
+
+public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
+{
+    public void Configure(EntityTypeBuilder<UserEntity> builder)
+    {
+        builder.ToTable("users");
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasColumnName("id").ValueGeneratedNever();
+        builder.Property(u => u.Email).HasColumnName("email").IsRequired().HasMaxLength(100);
+        builder.Property(u => u.FirstName).HasColumnName("first_name").IsRequired().HasMaxLength(50);
+        builder.Property(u => u.LastName).HasColumnName("last_name").IsRequired().HasMaxLength(50);
+        builder.Property(u => u.College).HasColumnName("college").HasMaxLength(100);
+        builder.Property(u => u.University).HasColumnName("university").HasMaxLength(100);
+        builder.Property(u => u.TechStack).HasColumnName("tech_stack").HasMaxLength(255);
+        builder.Property(u => u.Program).HasColumnName("program").HasMaxLength(50);
+        builder.Property(u => u.Address).HasColumnName("address").HasMaxLength(255);
+        builder.HasIndex(u => u.Email).IsUnique();
+    }
+}
